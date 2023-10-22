55c60440b12c00810a63322c375246d0973e9eeb
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-10-22 10:44:55 -0300

    Update Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,55 @@
+import { siteConfig } from "@/config/site";
+
+import Link from "next/link";
+
+import CTA from "@/components/cta";
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import { MoveRight } from "lucide-react";
+import { Cases, NossoProcesso, SitePorAssinatura, SitePremium, Valores, Vantagens } from "./sections";
+
+
+
 export default function Home() {
   return (
-    <main className="container flex min-h-screen items-center justify-center">
-      <h1>Nome do projeto</h1>
+    <main className="flex flex-col items-start">
+      <header className="container pt-6 lg:flex lg:items-center lg:justify-between">
+        <>
+          <h1 className="sr-only">L20 Comunicações</h1>
+          <Link href={'/'}>
+            <Icons.logo className="h-8 lg:h-12" />
+          </Link>
+        </>
+
+        <Button className="hidden items-center gap-2 lg:inline-flex ">
+          <span>Enviar mensagem</span>
+          <MoveRight />
+        </Button>
+      </header>
+
+      {/* HERO */}
+      <section id='hero' className="container flex flex-col items-start py-20 lg:py-28">
+
+        <div className="flex max-w-3xl flex-col">
+          <h2 className="heading2">O seu estúdio de design especialista em sites incríveis</h2>
+          <p className="mt-6 text-lg">Equilibrando ciência e criatividade, criamos websites, produtos e marcas com personalidade, impactando clientes, pessoas e negócios.</p>
+          <Button asChild className="mt-10 w-max">
+            <a href={siteConfig.links.whatsapp} target="_blank">
+              Vamos construir um site?
+            </a>
+          </Button>
+
+        </div>
+      </section>
+      <>
+        <SitePorAssinatura />
+        <SitePremium />
+        <Cases />
+        <Vantagens />
+        <CTA />
+        <NossoProcesso />
+        <Valores />
+      </>
     </main>
   )
 }
