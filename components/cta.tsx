c7aade4b16b299783eb9f73e8bb155ee327c827a
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-10-23 16:28:14 -0300

    Update component CTA

--- components/cta.tsx
+++ components/cta.tsx
@@ -6,7 +6,7 @@ export default function CTA() {
     <div className="container">
       <div className="grid grid-cols-1 gap-20 rounded-3xl bg-[url('/cta-bg.png')] bg-cover bg-center bg-no-repeat px-6 py-24 md:grid-cols-[414px,1fr] md:items-center lg:px-16">
         <div className="flex flex-col space-y-6">
-          <h3 className="text-4xl font-bold lg:text-5xl">Precisa de um projeto personalizado?</h3>
+          <h3 className="text-4xl font-bold lg:text-5xl lg:leading-tight "><span className="block pb-2 text-lg font-medium">Não Espere Mais!</span> <span>Comece Seu Projeto Personalizado!</span></h3>
           <p className="mb-10">Conheça o nosso modelo de criação personalizada, trabalhamos em parceria com você e sua equipe, com processos otimizados, simples!</p>
 
           <Button variant={"secondary"} className="w-max" asChild>
