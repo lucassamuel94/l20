7fffc26cbe1f12eba244ff4f178762155589246f
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-10-24 14:05:57 -0300

    Create animation

---
+++ styles/animations.ts
@@ -0,0 +1,9 @@
+export const animations = {
+  openUp: { opacity: 0, y: -20 },
+  whileUp: { opacity: 1, y: 0 },
+  openDown: { opacity: 0, y: 20 },
+  whileDown: { opacity: 1, y: 0 },
+  openLeft: { opacity: 0, x: -20 },
+  whileLeft: { opacity: 1, x: 0 },
+}
+export const transition = { duration: 1 }
